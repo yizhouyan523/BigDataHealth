fb7371f7151080d1a03ce1319f686a2f3f33698b
yizhouyan523 <40505279+yizhouyan523@users.noreply.github.com>
2020-07-23 10:43:05 -0400

    Add files via upload

---
+++ HW2/lrsgd.py
@@ -0,0 +1,41 @@
+# Do not use anything outside of the standard distribution of python
+# when implementing this class
+import math 
+
+class LogisticRegressionSGD:
+    """
+    Logistic regression with stochastic gradient descent
+    """
+
+    def __init__(self, eta, mu, n_feature):
+        """
+        Initialization of model parameters
+        """
+        self.eta = eta
+        self.weight = [0.0] * n_feature
+        self.mu = mu
+
+    def fit(self, X, y):
+        """
+        Update model using a pair of training sample
+        """
+        sig = 1.0 / (1.0 + math.exp(-math.fsum((self.weight[f]*v for f, v in X))))
+        for f,v in X:
+            self.weight[f] = self.weight[f] + self.eta*v*(-sig+y)
+        for i,v in enumerate(self.weight):
+            self.weight[i] = self.weight[i] - 2*self.eta*self.mu*self.weight[i]
+
+
+
+    def predict(self, X):
+        """
+        Predict 0 or 1 given X and the current weights in the model
+        """
+        return 1 if self.predict_prob(X) > 0.5 else 0
+        
+
+    def predict_prob(self, X):
+        """
+        Sigmoid function
+        """
+        return 1.0 / (1.0 + math.exp(-math.fsum((self.weight[f]*v for f, v in X))))
